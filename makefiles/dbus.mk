a9f9b219c113a2848d53cd7e51e635fe3ef11a3e
Nick Chan <towinchenmi@gmail.com>
2022-07-02 14:50:47 +0800

    hmm

--- makefiles/dbus.mk
+++ makefiles/dbus.mk
@@ -20,20 +20,29 @@ ifneq ($(wildcard $(BUILD_WORK)/dbus/.build_complete),)
 dbus:
 	@echo "Using previously built dbus."
 else
-dbus: dbus-setup expat glib2.0 libx11 libsm libice
+dbus: dbus-setup expat glib2.0 libx11 libsm libice pcre2 zstd lz4 gnupg libxcb libxau libmd libxdmcp
 	cd $(BUILD_WORK)/dbus && autoreconf -fiv
 	cd $(BUILD_WORK)/dbus && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
 		--disable-doxygen-docs \
 		--disable-ducktype-docs \
 		--disable-xml-docs \
+		--disable-systemd \
+		--disable-selinux \
+		--disable-apparmor \
+		--disable-inotify \
 		--enable-launchd \
+		--enable-x11-autolaunch \
 		--disable-tests \
-		--with-x \
-		--with-launchd-agent-dir=$(MEMO_PREFIX)/Library/LaunchDaemons \
+		--with-launchd-agent-dir=$(MEMO_PREFIX)/Library/LaunchAgents \
 		--with-system-pid-file=$(MEMO_PREFIX)/var/run/dbus/pid \
 		--with-system-socket=$(MEMO_PREFIX)/var/run/dbus/system_bus_socket \
-		--with-dbus-session-bus-listen-address=unix:runtime=yes
+		--with-dbus-session-bus-listen-address=unix:runtime=yes \
+		--with-x \
+		--x-libraries=$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+		--x-includes=$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include \
+		have_x11=yes \
+		PKG_CONFIG=$(BUILD_WORK)/cross-pkg-config
 	+$(MAKE) -C $(BUILD_WORK)/dbus
 	+$(MAKE) -C $(BUILD_WORK)/dbus install \
 		DESTDIR=$(BUILD_STAGE)/dbus
@@ -47,7 +56,7 @@ dbus-package: dbus-stage
 
 	mkdir -p $(BUILD_DIST)/dbus/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec \
 		$(BUILD_DIST)/dbus-bin/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin \
-		$(BUILD_DIST)/dbus-daemon/{$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin,$(MEMO_PREFIX)/Library/LaunchDaemons,$(DBUS_PROFILE)} \
+		$(BUILD_DIST)/dbus-daemon/{$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin,$(MEMO_PREFIX)/Library/LaunchAgents,$(DBUS_PROFILE)} \
 		$(BUILD_DIST)/dbus-session-bus-common/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/dbus-1/services \
 		$(BUILD_DIST)/dbus-system-bus-common/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/dbus-1/system-services \
 		$(BUILD_DIST)/dbus-x11/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin \
@@ -65,7 +74,7 @@ dbus-package: dbus-stage
 	# dbus.mk Prep dbus-daemon
 	cp -a $(BUILD_STAGE)/dbus/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/{dbus-daemon,dbus-run-session} \
 		$(BUILD_DIST)/dbus-daemon/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-	cp -a $(BUILD_STAGE)/dbus/$(MEMO_PREFIX)/Library/LaunchDaemons \
+	cp -a $(BUILD_STAGE)/dbus/$(MEMO_PREFIX)/Library/LaunchAgents \
 		$(BUILD_DIST)/dbus-daemon/$(MEMO_PREFIX)/Library/
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 	cp -a $(BUILD_MISC)/dbus/dbus.sh $(BUILD_DIST)/dbus-daemon/etc/profile.d
