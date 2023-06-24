fb3f719a3794c6ef9396e2ed8cdb5b0a8895d357
Nick Chan <towinchenmi@gmail.com>
2023-06-24 20:47:16 +0800

    shell-cmds: fix w compile on simulator

--- makefiles/shell-cmds.mk
+++ makefiles/shell-cmds.mk
@@ -8,6 +8,10 @@ STRAPPROJECTS      += shell-cmds
 SHELL-CMDS_VERSION := 278
 DEB_SHELL-CMDS_V   ?= $(SHELL-CMDS_VERSION)-2
 
+ifneq (,$(findstring simulator,$(MEMO_TARGET)))
+SIMULATOR_W_LDFLAGS := -lresolv
+endif
+
 shell-cmds-setup: setup
 	$(call GITHUB_ARCHIVE,apple-oss-distributions,shell_cmds,$(SHELL-CMDS_VERSION),shell_cmds-$(SHELL-CMDS_VERSION))
 	$(call EXTRACT_TAR,shell_cmds-$(SHELL-CMDS_VERSION).tar.gz,shell_cmds-shell_cmds-$(SHELL-CMDS_VERSION),shell-cmds)
@@ -26,12 +30,13 @@ shell-cmds: shell-cmds-setup openpam
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/su su/su.c -D'__FBSDID(x)=' $(LDFLAGS) -lpam; \
 	cp -a su/su.1 $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1; \
 	cp -a $(BUILD_MISC)/pam/su $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)/etc/pam.d; \
-	for bin in killall renice script time which getopt what jot apply lastcomm systime shlock systime w whereis; do \
+	for bin in killall renice script time which getopt what jot apply lastcomm systime shlock systime whereis; do \
 		echo $$bin; \
 		$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/$$bin $$bin/*.c -D'__FBSDID(x)=' -DHAVE_UTMPX=1 -save-temps; \
 		cp -a $$bin/$$bin.1 $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1 2>/dev/null; \
 		cp -a $$bin/$$bin.8 $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man8 2>/dev/null; \
 	done; \
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/w w/*.c -D'__FBSDID(x)=' -DHAVE_UTMPX=1 -save-temps $(SIMULATOR_W_LDFLAGS); \
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/path_helper path_helper/path_helper.c -save-temps; \
 	cp -a path_helper/path_helper.8 $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man8;
 	$(call AFTER_BUILD)
